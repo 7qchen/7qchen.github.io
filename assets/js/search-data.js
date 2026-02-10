f15fc7c7f116f31af19b6eb92f49829646518575
7qchen <7qchen@gmail.com>
2026-02-10 23:23:33 +0000

    Deploying to gh-pages from @ 7qchen/7qchen.github.io@adcfd8094ebe12690116d593eed4a0a2180c105d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-misc",
           title: "misc",
-          description: "Miscellaneous.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/misc/";
